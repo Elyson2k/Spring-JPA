7de54078370cc117793835ff7c1c011972f32b3a
Elyson2k <elysonvinicius@outlook.com>
2022-09-25 13:16:21 -0300

    Projeto criado

---
+++ src/test/java/com/vini/demo/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vini.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
